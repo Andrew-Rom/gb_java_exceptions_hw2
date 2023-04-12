84a523efd67554a37c5341884e665cd3dccbc837
Andrew-Rom <1811mail@gmail.com>
2023-04-12 20:37:38 +0300

    Completed task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /*
+          HW2. Task1
+         Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+         и возвращает введенное значение.
+         Ввод текста вместо числа не должно приводить к падению приложения,
+         вместо этого, необходимо повторно запросить у пользователя ввод данных.
+         */
+
+        System.out.println(">> " + askFloat());
+
+    }
+
+    private static float askFloat() {
+        boolean askUser = true;
+        float number = 0;
+        while (askUser) {
+            System.out.print("Enter the float number > ");
+            try {
+                Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+                if (!scanner.hasNextFloat()) {
+                    throw new IllegalArgumentException("Incorrect Input");
+                }
+
+                number = scanner.nextFloat();
+                askUser = false;
+                scanner.close();
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Unknown error");
+            }
+        }
+        return number;
     }
 }
